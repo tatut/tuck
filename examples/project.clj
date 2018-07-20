0dcc4fd320873c164fb1d657373cc54ca1ed9f6e
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-07-20 21:52:31 +0300

    Upgrade figwheel

--- examples/project.clj
+++ examples/project.clj
@@ -2,11 +2,11 @@
   :description "Tuck: a simple helper for UI folding"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.10.238"]
-                 [figwheel "0.5.15"]
+                 [figwheel "0.5.16"]
                  [reagent "0.8.0-alpha2"]
                  [cljs-ajax "0.7.3"]]
   :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-figwheel "0.5.15"]]
+            [lein-figwheel "0.5.16"]]
 
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src" "../src"]
